ee9a01b6043ee3e5ad6e5905c00c07b6f17669df
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-03-24 11:12:44 +0800

    🍁 update

--- prac/src/main.rs
+++ prac/src/main.rs
@@ -3,6 +3,14 @@ struct Number {
     value: i32,
 }
 
+struct NumRef<'a> {
+    x: &'a i32,
+}
+
+struct Person {
+    name: &'static str,
+}
+
 /* self trait */
 trait Signed {
     fn is_negative(&self) -> bool;
@@ -48,11 +56,20 @@ fn invert(n: &mut Number) {
     n.value = -n.value
 }
 
-fn compare<T>(l: T, r: T)
+// when single input lifetime, no need to specifty lifetime
+fn number_value<'a>(n: &'a Number) -> &'a i32 {
+    &n.value
+}
+
+fn compare<T>(lhs: T, rhs: T)
 where
     T: std::fmt::Debug + PartialEq,
 {
-    println!("{:?} {} {:?}", l, if l == r { "==" } else { "!=" }, r)
+    println!("{:?} {} {:?}", lhs, if lhs == rhs { "==" } else { "!=" }, rhs)
+}
+
+fn tail(s: &[u8]) -> &[u8] {
+    &s[1..]
 }
 
 fn main() {
@@ -83,4 +100,46 @@ fn main() {
 
     compare("wesley", "chen");
     compare("wesley", "wesley");
+
+    // NOTE: Option unwrap if something return it's content, nothing it panic
+    // NOTE: Result unwrap if OK return it's content, err it panic
+    let o1: Option<i32> = Some(12);
+    let a = o1.unwrap();
+    println!("a: {}", a);
+
+    let o1: Option<i32> = None;
+    // let a = o1.unwrap();
+    println!("o1: {:?}, a: {}",o1, a);
+
+    let mut n = Number { value: 47, odd: false };
+    {
+        let v = number_value(&n);
+        println!("{}", v);
+    }
+    n = Number{ value: 12, ..n };
+    println!("{}", n.value);
+
+    let x = 12;
+    let a = NumRef{ x: &x };
+    println!("{}", a.x);
+
+    let p = Person {
+        name: "Wesley"
+    };
+    println!("p: {}", p.name);
+
+    // Error
+    /* let name = format!("Wesley {}", "Chen"); */
+    /* p = Person { name: &name }; */
+
+    // silice
+    assert!((0..).contains(&100000));
+    assert!(!(..20).contains(&20));
+    assert!((..=20).contains(&20));
+    assert!((3..6).contains(&3));
+
+    let x = vec![1,2,3,4];
+    let y = tail(&x);
+    println!("y = {:?}", y);
+
 }
